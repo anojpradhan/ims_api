a919be525f28d215cbe680eb2414180e8b30f2ba
Anoz Pradhan <anozshrestha05@gmail.com>
2024-12-09 15:28:46 +0545

    Authentication Completed

--- src/app.module.ts
+++ src/app.module.ts
@@ -2,13 +2,14 @@ import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { RolesModule } from './roles/roles.module';
-// import { PrismaService } from './prisma/prisma.service';
 import { PrismaModule } from './prisma/prisma.module';
 import { OrganizationsModule } from './organizations/organizations.module';
 import { UsersModule } from './users/users.module';
+import { AuthModule } from './auth/auth.module';
+import { ConfigModule } from '@nestjs/config';
 
 @Module({
-  imports: [RolesModule, PrismaModule, OrganizationsModule, UsersModule],
+  imports: [ConfigModule.forRoot(),RolesModule, PrismaModule, OrganizationsModule, UsersModule, AuthModule],
   controllers: [AppController],
   providers: [AppService],
 })
---
+++ src/auth/auth.service.ts
@@ -0,0 +1,52 @@
+import { BadRequestException, Injectable, NotFoundException } from '@nestjs/common';
+import { PrismaService } from 'src/prisma/prisma.service';
+import { LoginDto, RegisterDto } from './auth.controller';
+// import {RegisterDto } from './auth.controller';
+import { UsersService } from 'src/users/users.service';
+import { JwtService } from '@nestjs/jwt';
+import { compare } from 'bcrypt';
+
+@Injectable()
+export class AuthService {
+  constructor(private readonly prismaService: PrismaService,
+    private readonly usersService:UsersService ,
+    private readonly jwtService:JwtService,
+  ) {}
+  async register(registerDto: RegisterDto) {
+    const user= await this.usersService.create(registerDto);
+    const token= await this.jwtService.signAsync({
+      user_id:user.id,
+      role_id:user.role_id,
+      organization_id:user.organization_id,
+    });
+    return {token};
+  }
+  async login(loginDto: LoginDto) {
+    // const user= await this.usersService.create(loginDto);
+    const user= await this.prismaService.user.findFirst({
+      where:{
+        OR:[{
+          email: loginDto.username,
+        },
+      {
+        mobile:loginDto.username,
+      }],
+      }
+    });
+    if(!user){
+      throw new NotFoundException(`user ${loginDto.username} Not Found `);
+    }
+    if(!(await compare(loginDto.password, user.password)))
+      {
+      throw new NotFoundException(`Invalid Credentials`);
+
+    }
+      const token= await this.jwtService.signAsync({
+        user_id:user.id,
+        role_id:user.role_id,
+        organization_id:user.organization_id,
+      });
+      return {token};
+    }
+  }
+
